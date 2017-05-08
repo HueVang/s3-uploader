147b50a2b8e999e7746684a2cb9469a0cbd4cd09
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-05-08 16:21:21 -0500

    Can upload files above 300MB now

--- apple.js
+++ apple.js
@@ -3,17 +3,23 @@ var express = require('express'),
     aws = require('aws-sdk'),
     bodyParser = require('body-parser'),
     multer = require('multer'),
+    progress = require('progress-stream'),
     multerS3 = require('multer-s3');
 
+
 require('dotenv').config()
+
+
 aws.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   region: process.env.AWS_REGION
 });
 
+
+
 var app = express(),
-    s3 = new aws.S3();
+    s3 = new aws.S3({logger: console});
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -24,23 +30,37 @@ var upload = multer({
         bucket: process.env.S3_BUCKET,
         contentType: multerS3.AUTO_CONTENT_TYPE, //Make the content type image/jpeg so the AWS S3 link is viewable
         key: function (req, file, cb) {
-            console.log('This is the file:', file);
+            console.log('This is the file:', file.originalname);
             cb(null, file.originalname); //use Date.now() for unique file keys
         },
         acl: 'public-read' //Change permission so that anyone can view the file
     })
 });
 
+
 //open in browser to see upload form
 app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
 
-//use by upload form
+// use by upload form
 app.post('/upload', upload.any(), function (req, res, next) {
-    res.send("Uploaded!");
+  // -------
+  var p = progress();
+req.pipe(p);
+p.headers = req.headers;
+p.on('progress', function (progress) {
+  var dates = new Date();
+  console.log('Finished at ' + dates.toString());
+});
+  // -------
+  var datez = new Date();
+    res.send("Uploaded " + req.files.length + " files! At " + datez.toString());
 });
 
-app.listen(3000, function () {
+
+var server = app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+server.timeout = 300000; // The server waits 5 minutes before it times out a request.
